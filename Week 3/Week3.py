95001cf621236fe6b766b1919fc860d260176515
Jacob Tang <tngzjn54@gmail.com>
2025-09-11 12:33:36 -0500

    First code to commit ! If statements !

--- Week 3/Week3.py
+++ Week 3/Week3.py
@@ -1,2 +1,13 @@
 # Created by Jacob Tang On sept 11 2025
 
+standards = int(input("Enter your standard: "))
+
+
+if standards >= 53:
+    print("A")
+if standards >= 51 and standards < 53: 
+    print("A-")
+if standards >= 49 and standards < 51:
+    print("B+")
+
+
