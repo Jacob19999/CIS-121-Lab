bedc830ee9b6ec5f92a11dbb0e614fb05a966ed6
Jacob Tang <tngzjn54@gmail.com>
2025-09-18 12:21:14 -0500

    Question 4 done!

--- Week 4/Week4.py
+++ Week 4/Week4.py
@@ -1,6 +1,7 @@
 # Jacob Tang
 
 '''
+
 larger_num = int(input("Enter the larger Number : "))
 smaller_num = int(input("Enter the larger Number : "))
 
@@ -10,14 +11,34 @@ while larger_num > smaller_num:
     num += 1
 
 print(f"Number of times halved: {num} ")
-'''
+
 
 
 # Question 2
 
 word = input("Enter a word: ")
 result = ""
+# First start point, Ending Point, step szie
 for i in range(1 , len(word) - 1,2):
     result += word[i]
 
 print(f"Output = {result}")
+
+'''
+
+# Question 4
+word = ""
+
+# While loop that runs forever
+while True:
+    # Read the user input
+    user_in = input("Enter a letter: ")
+    # if the user typed "done" we stop !
+    if user_in == "done":
+        break
+    else:
+        # Else lets add letter into the word
+        word += user_in
+
+# Print out the final word
+print(f"The final word is {word}")
