8ee4ad550f7342baaf507a0ccff72c92bfb7701f
Jacob Tang <tngzjn54@gmail.com>
2025-09-21 15:42:03 -0500

    push

--- Week 4/Week4.py
+++ Week 4/Week4.py
@@ -3,9 +3,10 @@
 # Hints:
 
 # While true run forever
-while True:
-    # Use break to stop the loop!
-    break
+done = False
+while not done:
+    # Use flag to stop the loop!
+    done = True
 
 word = "banana"
 i = 0
@@ -15,9 +16,9 @@ while i < len(word):
     i += 1
 
 # For loop (Starting Position, Ending Position, Step size)
-# Counts from 0 - 9
-for j in range(0, 10, 1):
-    break
+# Counts from 0 - 9 (0,2,4,6,8)
+for j in range(0, 10, 2):
+    pass #ignore this word for now...
 
 # Iterates through each letter of "banana"
 for letter in word:
@@ -27,11 +28,6 @@ for letter in word:
 # Counts from 0 to Word length - 1
 for i in range(0, len(word)):
     print(word[i])
-
-
-
-
-
 
 
 
