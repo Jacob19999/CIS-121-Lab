0b6afa94f607fe4f3dd168e47a166b573e504d95
Jacob Tang <tngzjn54@gmail.com>
2025-10-30 12:38:57 -0500

    updated question 6

--- Week 10/Week10.py
+++ Week 10/Week10.py
@@ -10,20 +10,22 @@ class Product:
     # Get Method
     def get_price(self):
         return self.price
+    
+    # Gets Price in Canadian dollors :(
+    def get_price_cad(self):
+        return self.price * 1.40
+    
     # Set Method
     def set_price(self, value):
         if value > 0:
             self.price = value
-
     # Get Method
     def get_quantity(self):
         return self.quantity
-    
     # Set Method
     def set_quantity(self, value):
         if 0 <= value <99:
             self.quantity = value
-
     # Build in print method 
     def __str__(self):
         return f"The product is {self.name} and priced at {self.price} we have {self.quantity}"
@@ -33,7 +35,7 @@ apple_iphone = Product("Iphone 17 Pro Max")
 apple_watch = Product("Apple Watch Series 7")
 # set the product price
 apple_iphone.set_price(1499)
-apple_watch.set_quantity(499)
+apple_watch.set_price(499)
 # Create the product quantity
 apple_iphone.set_quantity(5)
 apple_watch.set_quantity(5)
@@ -41,4 +43,41 @@ apple_watch.set_quantity(5)
 print(apple_iphone)
 print(apple_watch)
 
+# Question 6
+class Student:
+    def __init__(self):
+        self.name = "Unknown"
+        self.major = "Unknown"
+        self.gpa = 0
+    
+    def get_name(self):
+        return self.name
+    def set_name(self,value):
+        self.name = value
 
+    def get_major(self):
+        return self.major
+    def set_major(self, value):
+        self.major = value
+    
+    def get_gpa(self):
+        return self.gpa
+    def set_gpa(self, value):
+        if 0 <= value <=4.0:
+            self.gpa = value
+    
+    def introduce(self):
+        return f"Hi, i'm {self.name}. I'm studying {self.major}"
+    
+    def study_for_exam(self):
+        if self.gpa < 4.0:
+            self.gpa += 0.2
+
+student1 = Student()
+student1.set_name = "Evan Linder"
+student1.set_major = "Computer Science"
+student1.set_gpa = 3.8
+student1.study_for_exam()
+
+print(student1.introduce())
+print(student1.get_gpa())
